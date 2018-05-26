c069d181182af91ddf9829370fac1b0559617313
Dmitry Chertok <chertokdmitry@yandex.ru>
2018-05-26 16:55:51 +0300

    V38

--- src/games/BrainBalance.php
+++ src/games/BrainBalance.php
@@ -5,37 +5,36 @@ use function \cli\line;
 use function \cli\prompt;
 use function \BrainGames\Cli\run;
 
-function balance($arr, $lastkey)
-    {
-        sort($arr);
-        $last = end($arr);
-        if (($arr[$lastkey] - $arr[0]) < 2) {
-            $digit = implode($arr);
-            return  $digit;
-        } else {
-            $arr[0]++;
-            $arr[$lastkey]--;
-            return balance($arr, $lastkey);
-        }
-    }
+const GAME_TASK = 'Balance the given number?';
 
-function test()
+function balance($arr, $lastkey)
 {
-    define("GAME_TASK", "Balance the given number.");
-    
+    sort($arr);
+    $last = end($arr);
+    if (($arr[$lastkey] - $arr[0]) < 2) {
+        $digit = implode($arr);
+        return  $digit;
+    } else {
+        $arr[0]++;
+        $arr[$lastkey]--;
+        return balance($arr, $lastkey);
+    }
+}
+
+function runBalance()
+{
     $func = function () {
 
-    echo $num = rand(100, 10000);
-    $numArray = (str_split($num));
-    $lastkey = sizeof($numArray)-1;
-    foreach ($numArray as $key => $value) {
-        settype($numArray[$key], "int");
-    }
+        echo $num = rand(100, 10000);
+        $numArray = (str_split($num));
+        $lastkey = sizeof($numArray)-1;
+        foreach ($numArray as $key => $value) {
+            settype($numArray[$key], "int");
+        }
 
-    $digit = balance($numArray, $lastkey);
-    echo $digit;
-    return [$digit, $num];
+        $digit = balance($numArray, $lastkey);
+        return [$digit, $num];
     };
 
-    run($func);
+    run($func, GAME_TASK);
 }
