55f0f7008da213822eafe2c92ad28ec0d49f6c37
Dmitry Chertok <chertokdmitry@yandex.ru>
2018-05-26 15:13:37 +0300

    V36

--- src/games/BrainGcd.php
+++ src/games/BrainGcd.php
@@ -10,26 +10,26 @@ function runGcd()
     define("GAME_TASK", "Find the greatest common divisor of given numbers.");
 
     $func = function () {
-            $findDivisorFunc = function ($a, $b) use (&$findDivisorFunc) {
+            $findDivisor = function ($a, $b) use (&$findDivisor) {
                 $large = $a > $b ? $a: $b;
                 $small = $a > $b ? $b: $a;
                 $remainder = $large % $small;
-                return 0 == $remainder ? $small : $findDivisorFunc($small, $remainder);
+                return 0 == $remainder ? $small : $findDivisor($small, $remainder);
             };
 
         $num1 = rand(1, 100);
         $num2 = rand(1, 100);
-        $divisor = $findDivisorFunc($num1, $num2);
+        $divisor = $findDivisor($num1, $num2);
 
         if ($divisor < 2) {
             while ($divisor < 2) {
                 $num1 = rand(1, 100);
                 $num2 = rand(1, 100);
-                $divisor = $findDivisorFunc($num1, $num2);
+                $divisor = $findDivisor($num1, $num2);
             }
         }
-        $questionString = $num1 . ' ' . $num2;
-        return [$divisor, $questionString];
+        $question = $num1 . ' ' . $num2;
+        return [$divisor, $question];
     };
 
     run($func);
